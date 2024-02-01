12309d823939c390ba492b9d0a1980444055a9f9
Shru Kumar <shrutika.skumar@gmail.com>
2024-02-01 11:42:25 -0500

    initial files from HW1

---
+++ tweet_object.py
@@ -0,0 +1,15 @@
+"""
+author: Shru Kumar
+filename: tweet_object.py
+description: establishing class Tweet and its store data
+"""
+class Tweet:
+
+    def __init__(self, user_id, tweet_text):
+        self.user_id = user_id
+        self.tweet_text = tweet_text
+
+    def __str__(self):
+        return f"{self.tweet_text} - user {self.user_id} at {self.tweet_ts}"
+        
+
