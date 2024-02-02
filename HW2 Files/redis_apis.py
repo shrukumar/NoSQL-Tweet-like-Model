de661c706505d4d9629bc261d2239cfb27483172
Shru Kumar <shrutika.skumar@gmail.com>
2024-02-02 00:34:42 -0500

    strategy 1 and 2 implemented

--- HW2 Files/redis_apis.py
+++ HW2 Files/redis_apis.py
@@ -13,28 +13,41 @@ class TwitterAPI1:
     def populate_followers(self, file):
         df_followers = pd.read_csv(file)
         for idx, row in df_followers.iterrows():
-            self.r.lpush(f'user:{int(row[1])}', int(row[0]))
+            self.r.lpush(f'follow_user:{int(row[0])}', int(row[1]))
 
     def post_tweet(self, tweet):
         self.r.hset(f'tweet:{tweet.tweet_id}', mapping={'user': tweet.user_id, 'text': tweet.tweet_text, \
                                                            'time': tweet.tweet_ts})
     def get_random_user(self):
-        users = self.r.keys('user:*')
+        users = self.r.keys('follow_user:*')
         user_ids = []
         for user in users:
             user_ids.append(user.split(':')[1])
         rand_user = random.choice(user_ids)
         return rand_user
+
+    def map_tweets_users(self):
+        tweet_ids = self.r.keys('tweet:*')
+        for tweet in tweet_ids:
+            self.r.lpush(f'user_id:{self.r.hget(tweet, "user")}', tweet)
+
     def get_timeline(self):
-        rand_user = self.get_random_user()
-        following = self.r.lrange(f'user:{rand_user}', 0, -1)
+        # rand_user = self.get_random_user()
+        rand_user = 1
+        # print(rand_user)
+        following = self.r.lrange(f'follow_user:{rand_user}', 0, -1)
+        # print(following)
 
         for user in following:
-            self.r.hmget()
+            # print(user)
+            for tweet_id in self.r.lrange(f'user_id:{user}', 0, -1):
+                text = self.r.hget(tweet_id, 'text')
+                user = self.r.hget(tweet_id, 'user')
+                time = self.r.hget(tweet_id, 'time')
+                self.r.zadd('timeline', {f'User {user} tweeted "{text}" at {time}.': time})
 
-
-
-
+        timeline = self.r.zrevrange('timeline', 0, 9)
+        return rand_user, timeline
 
 class TwitterAPI2:
 
@@ -48,22 +61,22 @@ class TwitterAPI2:
     def populate_followers(self, file):
         df_followers = pd.read_csv(file)
         for idx, row in df_followers.iterrows():
-            self.r.lpush(f'user:{int(row[1])}', int(row[0]))
+            self.r.lpush(f'follow_user:{int(row[1])}', int(row[0]))
 
     def post_tweet(self, tweet):
         self.r.hset(f'tweet_id:{tweet.tweet_id}', mapping = {'user': tweet.user_id, 'text': tweet.tweet_text, \
                                                              'time': tweet.tweet_ts})
-        # new_tweet = self.r.hgetall(f'tweet_id:{tweet.tweet_id}')
+
         text = self.r.hget(f'tweet_id:{tweet.tweet_id}', 'text')
         user = self.r.hget(f'tweet_id:{tweet.tweet_id}', 'user')
         time = self.r.hget(f'tweet_id:{tweet.tweet_id}', 'time')
 
 
-        for follower in self.r.lrange(f'user:{tweet.user_id}', 0, -1):
+        for follower in self.r.lrange(f'follow_user:{tweet.user_id}', 0, -1):
             self.r.lpush(f'timeline_for_user:{follower}', f'User {user} tweeted "{text}" at {time}.')
 
     def get_random_user(self):
-        users = self.r.keys('user:*')
+        users = self.r.keys('follow_user:*')
         user_ids = []
         for user in users:
             user_ids.append(user.split(':')[1])
--- HW2 Files/redis_tester1.py
+++ HW2 Files/redis_tester1.py
@@ -10,46 +10,48 @@ def main():
     # adding followers
     api.populate_followers('follows_sample.csv')
 
-    api.get_timeline()
-    # # start timer for posts
-    # start_time = time.time()
-    #
-    # # adding tweets
-    # df_tweets = pd.read_csv('tweets_sample.csv')
-    # for idx, row in df_tweets.iterrows():
-    #     tweet = Tweet(tweet_id = idx, user_id=row[0], tweet_text=row[1], tweet_ts = time.ctime())
-    #     api.post_tweet(tweet)
-    #
-    # # end timer for posts
-    # time_elapsed = time.time() - start_time
-    #
-    # # establish timer and counter for timelines
-    # time_elapsed1 = 0
-    # timeline_count = 0
-    #
-    # # start timer for timelines
-    # start_time1 = time.time()
-    # while time_elapsed1 < 6:
-    #     # retrieve timelines for random users
-    #     user, timeline = api.get_timeline()
-    #     # printing timelines tweet by tweet
-    #     print(f'Timeline for user {user}:')
-    #     for post in timeline:
-    #         print(post)
-    #     print()
-    #     timeline_count += 1
-    #
-    #     # while loop checks if time elapsed goes past target time
-    #     time_elapsed1 = time.time() - start_time1
-    #
-    # # close connection
-    # api.r.close()
-    #
-    # # results
-    # print(f"It took {time_elapsed} seconds to post {len(df_tweets)} tweets.")
-    # print(f"Made {round(len(df_tweets) / time_elapsed, 1)} post API calls per second")
-    #
-    # print(f"Retrieved {timeline_count} timelines in {time_elapsed1} seconds")
-    # print(f"Made {round(timeline_count / time_elapsed1, 2)} timeline API calls per second")
-    #
+    # start timer for posts
+    start_time = time.time()
+
+    # adding tweets
+    df_tweets = pd.read_csv('tweets_sample.csv')
+    for idx, row in df_tweets.iterrows():
+        tweet = Tweet(tweet_id = idx, user_id=row[0], tweet_text=row[1], tweet_ts = time.time())
+        api.post_tweet(tweet)
+
+    # mapping user_id keys to tweet_id values
+    api.map_tweets_users()
+
+    # end timer for posts
+    time_elapsed = time.time() - start_time
+
+    # establish timer and counter for timelines
+    time_elapsed1 = 0
+    timeline_count = 0
+
+    # start timer for timelines
+    start_time1 = time.time()
+    while time_elapsed1 < 6:
+        # retrieve timelines for random users
+        user, timeline = api.get_timeline()
+        # printing timelines tweet by tweet
+        print(f'Timeline for user {user}:')
+        for post in timeline:
+            print(post)
+        print()
+        timeline_count += 1
+
+        # while loop checks if time elapsed goes past target time
+        time_elapsed1 = time.time() - start_time1
+
+    # close connection
+    api.r.close()
+
+    # results
+    print(f"It took {time_elapsed} seconds to post {len(df_tweets)} tweets.")
+    print(f"Made {round(len(df_tweets) / time_elapsed, 1)} post API calls per second")
+
+    print(f"Retrieved {timeline_count} timelines in {time_elapsed1} seconds")
+    print(f"Made {round(timeline_count / time_elapsed1, 2)} timeline API calls per second")
+
 main()
